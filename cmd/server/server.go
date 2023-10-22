062bcd59056bff03cfe4d65f94f0a84f87e8e318
Michael Chi <chicong@outlook.com>
2023-10-22 14:49:26 +0800

    Add gin

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"github.com/comoyi/sparrow/config"
+	"github.com/gin-gonic/gin"
 	"log/slog"
 	"net/http"
 	_ "net/http/pprof"
@@ -23,6 +24,8 @@ func start() {
 
 	go startPprof()
 
+	go startHttpServer()
+
 	blockChan := make(chan struct{})
 	<-blockChan
 }
@@ -33,4 +36,43 @@ func startPprof() {
 	if err != nil {
 		slog.Warn(fmt.Sprintf("err: %v", err))
 	}
+}
+
+func startHttpServer() {
+	addr := fmt.Sprintf(":%d", config.Conf.Port)
+
+	engine := gin.New()
+	engine.Use(gin.Logger(), gin.Recovery())
+
+	initRouter(engine)
+
+	//err := engine.Run(addr)
+	//if err != nil {
+	//	slog.Error("http server start sailed")
+	//}
+
+	s := &http.Server{
+		Addr:    addr,
+		Handler: engine.Handler(),
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		slog.Error("http server start sailed")
+		return
+	}
+}
+
+func initRouter(engine *gin.Engine) {
+	ga := engine.Group("/ga")
+	ga.GET("/a", func(context *gin.Context) {
+		context.JSON(200, "/ga/a")
+	})
+	ga.GET("/b", func(context *gin.Context) {
+		context.JSON(200, "/ga/b")
+	})
+
+	gb := engine.Group("/gb")
+	gb.GET("/a", func(context *gin.Context) {
+		context.JSON(200, "/gb/a")
+	})
 }
